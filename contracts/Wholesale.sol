04942c80eac850061d166479fe69292c8e52c133
Alexey Gmitron <gmitron01@gmail.com>
2021-11-20 15:22:57 +0700

    refactor: accounts can have many roles

--- contracts/Wholesale.sol
+++ contracts/Wholesale.sol
@@ -11,43 +11,5 @@ contract Wholesale is Ownable {
     uint balance;
   }
 
-  mapping (address => address) public storesToSellersMap;
-  mapping (address => Store) public storesMap;
-  mapping (address => bool) public adminsMap;
-  mapping (address => bool) public sellersMap;
-  mapping (address => bool) public buyersMap;
-  mapping (address => bool) public guestsMap;
 
-  constructor() {
-    adminsMap[msg.sender] = true;
-  }
-
-  /// Only admins can call this method
-  error OnlyForAdmins();
-
-  modifier onlyAdmin() {
-    require(adminsMap[msg.sender], 'This method is allowed only for admins.');
-    _;
-  }
-
-  modifier guestGuard() {
-
-  }
-
-  function addStore(address account, string memory city) external onlyAdmin {
-    Store memory newStore = Store(city, 1000);
-    storesMap[account] = newStore;
-  }
-
-  function addAdmin(address account) external onlyAdmin {
-    adminsMap[account] = true;
-  }
-
-  function removeStore(address storeToRemove) external onlyAdmin {
-    delete storesMap[storeToRemove];
-  }
-
-  function promoteToSeller(address accountToPromote) external onlyAdmin {
-
-  }
 }
