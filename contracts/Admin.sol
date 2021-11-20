bac7f3ebbbf57544f8c186382117da04c6c4764e
Alexey Gmitron <gmitron01@gmail.com>
2021-11-20 16:44:48 +0700

    refactor: promotions requirements

--- contracts/Admin.sol
+++ contracts/Admin.sol
@@ -4,8 +4,30 @@ pragma solidity >=0.4.22 <0.9.0;
 import "./Roles.sol";
 
 contract AdminContract is Roles {
-  function promoteCustomerToSeller(address customerAccount) external onlyRole(RolesEnum.Admin) {
-    require(!accountRoles[customerAccount][RolesEnum.Admin], 'The user has already have admin rights.');
-    accountRoles[customerAccount][RolesEnum.Admin] = true;
-  }
+    function promoteCustomerToSeller(address customerAccount)
+        external
+        onlyRole(RolesEnum.Admin)
+    {
+        require(
+            !accountRoles[customerAccount][RolesEnum.Customer],
+            "The user must be a customer."
+        );
+        require(
+            !accountRoles[customerAccount][RolesEnum.Seller],
+            "The user has already have admin rights."
+        );
+        accountRoles[customerAccount][RolesEnum.Seller] = true;
+    }
+
+    function demoteSellerToCustomer(address customerAccount)
+        external
+        onlyRole(RolesEnum.Admin)
+    {
+        require(
+            accountRoles[customerAccount][RolesEnum.Seller],
+            "The user doesn't have seller rights."
+        );
+        accountRoles[customerAccount][RolesEnum.Seller] = false;
+        accountRoles[customerAccount][RolesEnum.Customer] = true;
+    }
 }
